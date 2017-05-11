eeb97a03bc34134803f0cee5fcc2697617a1edeb
yongpeishi <yongpeishi@gmail.com>
2017-05-12 01:02:08 +1000

    refactor: inline swap! function

--- frontend/src/frontend/controllers/ticket.cljs
+++ frontend/src/frontend/controllers/ticket.cljs
@@ -5,20 +5,18 @@
             [frontend.api-client :as api]))
 
 (defn set-ticket-number [event]
-  (let [value event.target.value]
-    (swap! app-state (fn [state]
-                       (m/set-number state value)))))
+  (swap! app-state m/set-number event.target.value))
 
 (defn handle-error-response [{:keys [status]}]
   (swap! app-state m/update-page-with-error status))
 
+(defn handle-success-response [response]
+  (swap! app-state m/update-ticket-details response))
+
 (defn get-ticket []
   (go
-    (let [number         (get-in @app-state [:ticket :number-entered])
+    (let [number        (get-in @app-state [:ticket :number-entered])
           [ok response] (<! (api/get-ticket number))]
       (if ok
-        (swap! app-state (fn [state]
-                           (m/update-ticket-details state response)))
+        (handle-success-response response)
         (handle-error-response response)))))
-
-
